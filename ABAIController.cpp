573dc6b4c34c72c74d7f273a5a5352b8a96c183d
fistki <38302858+fistki@users.noreply.github.com>
2021-05-20 20:26:43 +0900

    CHAPTER 12. 비헤이비어 트리 시스템
    * 비헤이비어 트리 제작
    * AIController가 비헤이비어 트리에 따라 동작함
    * 비헤이비어 트리는 블랙보드의 데이터를 이용하여 움직임
    * FindPatrolPos라는 테스크를 만들어 랜덤으로 위치값을 얻음
    * 비헤이비어 트리에서 FindPatrolPos, Wait, MoveTo 테스크로 5초마다 랜덤한 위치로 이동하는 로직 구현

--- ABAIController.cpp
+++ ABAIController.cpp
@@ -2,37 +2,37 @@
 
 
 #include "ABAIController.h"
-#include "NavigationSystem.h"
-#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
-AABAIController::AABAIController(): RepeatInterval(3.0f)
+const FName AABAIController::HomePosKey(TEXT("HomePos"));
+const FName AABAIController::PatrolPosKey(TEXT("PatrolPos"));
+
+AABAIController::AABAIController()
 {
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BTObject(TEXT("/Game/Book/AI/BT_ABCharacter.BT_ABCharacter"));
+	if (BTObject.Succeeded())
+	{
+		BTAsset = BTObject.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BBObject(TEXT("/Game/Book/AI/BB_ABCharacter.BB_ABCharacter"));
+	if (BBObject.Succeeded())
+	{
+		BBAsset = BBObject.Object;
+	}
 }
 
 void AABAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
-	GetWorld()->GetTimerManager().SetTimer(RepeatTimerHandle, this, &AABAIController::OnRepeatTimer, RepeatInterval, true);
-}
-
-void AABAIController::OnUnPossess()
-{
-	Super::OnUnPossess();
-	GetWorld()->GetTimerManager().ClearTimer(RepeatTimerHandle);
-}
-
-void AABAIController::OnRepeatTimer()
-{
-	auto CurrentPawn = GetPawn();
-	ABCHECK(nullptr != CurrentPawn);
-
-	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
-	if (nullptr == NavSystem) return;
-	
-	FNavLocation NextLocation;
-	if (NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.0f, NextLocation))
+	if (UseBlackboard(BBAsset, Blackboard))
 	{
-		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, NextLocation.Location);
-		ABLOG(Warning, TEXT("Next Location : %s"), *NextLocation.Location.ToString());
+		Blackboard->SetValueAsVector(HomePosKey, InPawn->GetActorLocation());
+		if (!RunBehaviorTree(BTAsset))
+		{
+			ABLOG(Error, TEXT("AIController couldn't run behavior tree!"));
+		}
 	}
 }
