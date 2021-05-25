b94da93a0ca31ca5d35c4f340ecacf064627d066
fistki <38302858+fistki@users.noreply.github.com>
2021-05-25 16:01:59 +0900

    CHAPTER 15. 게임데이터의 저장과 로딩
    * SaveGame 클래스를 구현
    * PlayerState에서 세이브파일을 슬롯이름을 기준으로 불러옴
    * 플레이어 데이터(PlayerState)가 변경될 때 마다 세이브함(SavePlayerData())

--- Private/ABPlayerState.cpp
+++ Private/ABPlayerState.cpp
@@ -3,9 +3,10 @@
 
 #include "ABPlayerState.h"
 #include "ABGameInstance.h"
+#include "ABSaveGame.h"
 
 AABPlayerState::AABPlayerState()
-	: GameScore(0), CharacterLevel(1), Exp(0)
+	: GameScore(0), GameHighScore(0), CharacterLevel(1), Exp(0), SaveSlotName(TEXT("Player1"))
 {
 }
 
@@ -13,6 +14,12 @@ int32 AABPlayerState::GetGameScore() const
 {
 	return GameScore;
 }
+
+int32 AABPlayerState::GetGameHighScore() const
+{
+	return GameHighScore;
+}
+
 int32 AABPlayerState::GetCharacterLevel() const
 {
 	return CharacterLevel;
@@ -47,21 +54,49 @@ bool AABPlayerState::AddExp(int32 IncomeExp)
 	}
 
 	OnPlayerStateChanged.Broadcast();
+	SavePlayerData();
 	return DidLevelUp;
 }
 
 void AABPlayerState::AddGameScore()
 {
 	GameScore++;
+	if (GameScore >= GameHighScore)
+	{
+		GameHighScore = GameScore;
+	}
 	OnPlayerStateChanged.Broadcast();
+	SavePlayerData();
 }
 
 void AABPlayerState::InitPlayerData()
 {
-	SetPlayerName(TEXT("Fistki"));
-	SetCharacterLevel(5);
+	auto ABSaveGame = Cast<UABSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlotName, 0));
+	if (nullptr == ABSaveGame)
+	{
+		ABSaveGame = GetMutableDefault<UABSaveGame>();
+	}
+
+	SetPlayerName(ABSaveGame->PlayerName);
+	SetCharacterLevel(ABSaveGame->Level);
 	GameScore = 0;
-	Exp = 0;
+	GameHighScore = ABSaveGame->HighScore;
+	Exp = ABSaveGame->Exp;
+	SavePlayerData();
+}
+
+void  AABPlayerState::SavePlayerData()
+{
+	UABSaveGame* NewPlayerData = NewObject<UABSaveGame>();
+	NewPlayerData->PlayerName = GetPlayerName();
+	NewPlayerData->Level = CharacterLevel;
+	NewPlayerData->Exp = Exp;
+	NewPlayerData->HighScore = GameHighScore;
+
+	if (!UGameplayStatics::SaveGameToSlot(NewPlayerData, SaveSlotName, 0))
+	{
+		ABLOG(Error, TEXT("SaveGame Error!"));
+	}
 }
 
 void AABPlayerState::SetCharacterLevel(int32 NewCharacterLevel)
--- Public/ABPlayerState.h
+++ Public/ABPlayerState.h
@@ -20,13 +20,14 @@ public:
 	AABPlayerState();
 
 	int32 GetGameScore() const;
+	int32 GetGameHighScore() const;
 	int32 GetCharacterLevel() const;
 	float GetExpRatio() const;
 	bool AddExp(int32 IncomeExp);
 	void AddGameScore();
 
 	void InitPlayerData();
-
+	void SavePlayerData();
 	FOnPlayerStateChangedDelegate OnPlayerStateChanged;
 
 protected:
@@ -34,10 +35,16 @@ protected:
 	int32 GameScore;
 
 	UPROPERTY(Transient)
+	int32 GameHighScore;
+
+	UPROPERTY(Transient)
 	int32 CharacterLevel;
 
 	UPROPERTY(Transient)
 	int32 Exp;
+
+public:
+	FString SaveSlotName;
 
 private:
 	void SetCharacterLevel(int32 NewCharacterLevel);
